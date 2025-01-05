1c94f0ea8f3e035d1bac9573a45f8cdcf0a43b1a
jonlevinsky <124724335+jonlevinsky@users.noreply.github.com>
2025-01-05 19:02:14 +0100

    vault backup: 2025-01-05 19:02:14

--- .obsidian/plugins/celtx/main.js
+++ .obsidian/plugins/celtx/main.js
@@ -1,41 +1,9 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || (function () {
-    var ownKeys = function(o) {
-        ownKeys = Object.getOwnPropertyNames || function (o) {
-            var ar = [];
-            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
-            return ar;
-        };
-        return ownKeys(o);
-    };
-    return function (mod) {
-        if (mod && mod.__esModule) return mod;
-        var result = {};
-        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
-        __setModuleDefault(result, mod);
-        return result;
-    };
-})();
 Object.defineProperty(exports, "__esModule", { value: true });
-const obsidian_1 = require("obsidian");
-const path = __importStar(require("path")); // Importujte path z Node.js
-class CeltxLikePlugin extends obsidian_1.Plugin {
+const { Plugin, Modal, Notice, Editor, TFile } = require('obsidian');
+const fs = require('fs');
+const path = require('path');
+class CeltxLikePlugin extends Plugin {
     async onload() {
         console.log("CeltxLikePlugin loaded");
         // Přidání klávesových zkratek a příkazů
@@ -60,18 +28,26 @@ class CeltxLikePlugin extends obsidian_1.Plugin {
     }
     async getLocationFiles() {
         const folderPath = 'Lokace'; // Cesta k složce s lokaci
+        await this.ensureLocationFolderExists(folderPath); // Zkontroluje, zda složka existuje, pokud ne, vytvoří ji
         const files = this.app.vault.getFiles().filter((file) => file.path.startsWith(folderPath));
         return files;
     }
     async createNewLocationFile(location) {
         const folderPath = 'Lokace'; // Cesta k složce s lokaci
+        await this.ensureLocationFolderExists(folderPath); // Zkontroluje, zda složka existuje, pokud ne, vytvoří ji
         const newFilePath = path.join(folderPath, `${location}.md`);
         const newFile = await this.app.vault.create(newFilePath, `# ${location}\n\n`);
         return newFile;
     }
+    async ensureLocationFolderExists(folderPath) {
+        const folderExists = await this.app.vault.adapter.exists(folderPath);
+        if (!folderExists) {
+            await this.app.vault.createFolder(folderPath); // Vytvoření složky, pokud neexistuje
+        }
+    }
 }
 exports.default = CeltxLikePlugin;
-class FormatIntExtModal extends obsidian_1.Modal {
+class FormatIntExtModal extends Modal {
     constructor(app, editor) {
         super(app);
         this.editor = editor;
@@ -95,14 +71,13 @@ class FormatIntExtModal extends obsidian_1.Modal {
         contentEl.empty();
     }
     async selectLocation(type) {
-        // Opravený přístup k metodě getLocationFiles
-        const locationFiles = await this.app.plugins.plugins['celtx'].getLocationFiles();
+        const locationFiles = await this.app.plugins.plugins['obsidian-core'].getLocationFiles();
         const locationNames = locationFiles.map((file) => path.basename(file.path, '.md'));
         const locationSelectionModal = new LocationSelectionModal(this.app, type, locationNames, this.editor);
         locationSelectionModal.open();
     }
 }
-class LocationSelectionModal extends obsidian_1.Modal {
+class LocationSelectionModal extends Modal {
     constructor(app, type, locationNames, editor) {
         super(app);
         this.type = type;
@@ -126,7 +101,7 @@ class LocationSelectionModal extends obsidian_1.Modal {
             if (selectedLocation && !this.locationNames.includes(selectedLocation)) {
                 await this.createNewLocation(selectedLocation);
             }
-            else if (selectedLocation) {
+            else {
                 await this.insertLocationText(selectedLocation);
             }
         };
@@ -140,8 +115,8 @@ class LocationSelectionModal extends obsidian_1.Modal {
         this.editor.replaceRange(text, this.editor.getCursor());
     }
     async createNewLocation(location) {
-        const newFile = await this.app.plugins.plugins['celtx'].createNewLocationFile(location);
+        const newFile = await this.app.plugins.plugins['obsidian-core'].createNewLocationFile(location);
         await this.insertLocationText(location);
-        new obsidian_1.Notice(`Created new location: ${newFile.path}`);
+        new Notice(`Created new location: ${newFile.path}`);
     }
 }
