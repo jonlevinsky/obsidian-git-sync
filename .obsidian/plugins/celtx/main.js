0e09aa83cf3501d9c7053a940c69e4d118cb428b
jonlevinsky <124724335+jonlevinsky@users.noreply.github.com>
2025-01-06 20:36:27 +0100

    vault backup: 2025-01-06 20:36:27

--- .obsidian/plugins/celtx/main.js
+++ .obsidian/plugins/celtx/main.js
@@ -1,12 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const script1_1 = require("./script1");
-const script2_1 = require("./script2");
-class MyPlugin extends Plugin {
-    onload() {
-        // Inicializace pluginu
-        (0, script1_1.someFunction)();
-        (0, script2_1.anotherFunction)();
-    }
-}
-exports.default = MyPlugin;
+(0, script1_1.someFunction)();
--- .obsidian/plugins/celtx/script1.js
+++ .obsidian/plugins/celtx/script1.js
@@ -1,8 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.someFunction = someFunction;
-// script1.ts
 function someFunction() {
-    console.log("Funkce z script1.ts byla zavolána.");
-    // Tady můžeš mít kód, který dělá něco specifického pro plugin
+    console.log("Funkce byla zavolána");
 }
