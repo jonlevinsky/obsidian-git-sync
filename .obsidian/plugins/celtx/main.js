387b55e5f6167091c0f4784a0db28debd5f0589b
jonlevinsky <124724335+jonlevinsky@users.noreply.github.com>
2025-01-06 20:46:21 +0100

    vault backup: 2025-01-06 20:46:21

--- .obsidian/plugins/celtx/main.js
+++ .obsidian/plugins/celtx/main.js
@@ -7,7 +7,6 @@ class MyPlugin extends Plugin {
     onload() {
         const location = (0, locations_1.addLocation)('Mysterious Forest', 'A dense and eerie forest.', '45.123, 23.456');
         const character = (0, characters_1.addCharacter)('Durkovič', 'Historian', 45);
-        console.log('Location:', (0, locations_1.getLocationName)(location));
         console.log('Character:', (0, characters_1.getCharacterName)(character));
     }
 }
