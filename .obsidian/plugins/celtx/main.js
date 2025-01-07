a4f69ece340a03054619a005e9bd0baa4eda0c9e
jonlevinsky <124724335+jonlevinsky@users.noreply.github.com>
2025-01-07 22:52:10 +0100

    Záloha ze 2025-01-07 22:52:10 z LevinskyJ Desktop

--- .obsidian/plugins/celtx/main.js
+++ .obsidian/plugins/celtx/main.js
@@ -1,57 +1,86 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const obsidian_1 = require("obsidian");
-const sceneHeadingRegex = /# (.*)/; // Pro # Scene Heading
-const actionRegex = /## (.*)/; // Pro ## Action
-const characterRegex = /### (.*)/; // Pro ### Character
-const parentheticalsRegex = /#### (.*)/; // Pro #### Parentheticals
-const dialogueRegex = /##### (.*)/; // Pro ##### Dialogue
-const transitionRegex = /###### (.*)/; // Pro ###### Transition
 class ScriptFormattingPlugin extends obsidian_1.Plugin {
     async onload() {
-        this.addCSS('style.css');
-        this.addCommand({
-            id: 'format-script',
-            name: 'Format Script',
-            callback: () => {
-                this.formatScript();
-            }
-        });
-    }
-    async formatScript() {
-        const activeView = this.app.workspace.activeLeaf?.view;
-        if (!(activeView instanceof obsidian_1.MarkdownView))
-            return; // Zkontroluje, zda je aktivní pohled typu MarkdownView
-        const activeFile = activeView.file; // Získá soubor z MarkdownView
-        if (!activeFile)
-            return;
-        const fileContent = await this.app.vault.read(activeFile); // Čtení souboru asynchronně
-        const formattedContent = this.generateFormattedText(fileContent);
-        // Automatické přepsání souboru novým formátovaným obsahem
-        await this.app.vault.modify(activeFile, formattedContent);
-    }
-    generateFormattedText(input) {
-        let output = input;
-        // Regex pro nahrazení tagů třídami podle předchozího kódu
-        output = output.replace(sceneHeadingRegex, (match, p1) => {
-            return `<div class="scene-heading">${p1}</div>`;
-        });
-        output = output.replace(actionRegex, (match, p1) => {
-            return `<div class="action">${p1}</div>`;
-        });
-        output = output.replace(characterRegex, (match, p1) => {
-            return `<div class="character">${p1}</div>`;
-        });
-        output = output.replace(parentheticalsRegex, (match, p1) => {
-            return `<div class="parentheticals">${p1}</div>`;
-        });
-        output = output.replace(dialogueRegex, (match, p1) => {
-            return `<div class="dialogue">${p1}</div>`;
-        });
-        output = output.replace(transitionRegex, (match, p1) => {
-            return `<div class="transition">${p1}</div>`;
-        });
-        return output;
+        const style = document.createElement('style');
+        style.textContent = `
+      /* Celkový styl pro scénář */
+      body {
+        font-size: 12pt;
+        line-height: 1; /* Mírně zvýšené řádkování pro lepší čitelnost */
+      }
+
+      /* Scene Heading (h1) - pro # */
+      h1 {
+        font-family: 'Courier New', Courier, monospace;
+        font-size: 12pt;
+        font-weight: bold;
+        text-transform: uppercase;
+        background-color: #D3D3D3; /* světle šedá */
+        color: #1e1e1e;
+        padding: 0.2in 0;
+        margin-bottom: 0.2in;
+        text-align: left;
+        letter-spacing: 1px;
+        line-height: 1;
+      }
+
+      /* Action (h2) - pro ## */
+      h2 {
+        font-family: 'Courier New', Courier, monospace;
+        font-size: 12pt;
+        line-height: 1;
+        margin-bottom: 0.5in;
+        text-align: justify;
+      }
+
+      /* Character (h3) - pro ### */
+      h3 {
+        font-family: 'Courier New', Courier, monospace;
+        font-size: 12pt;
+        font-weight: bold;
+        text-transform: uppercase;
+        text-align: left;
+        margin-left: 3.7in;
+        margin-bottom: 0.2in;
+        line-height: 1;
+      }
+
+      /* Parentheticals (h4) - pro #### */
+      h4 {
+        font-family: 'Courier New', Courier, monospace;
+        font-size: 12pt;
+        font-style: italic;
+        text-align: left;
+        margin-left: 3.7in;
+        margin-bottom: 0.2in;
+        line-height: 1;
+      }
+
+      /* Dialogue (h5) - pro ##### */
+      h5 {
+        font-family: 'Courier New', Courier, monospace;
+        font-size: 12pt;
+        line-height: 1;
+        text-align: left;
+        margin-left: 2.5in; /* Odsazení pro dialog */
+        margin-right: 1in; /* Pravý okraj */
+        margin-bottom: 0.5in;
+      }
+
+      /* Transition (h6) - pro ###### */
+      h6 {
+        font-family: 'Courier New', Courier, monospace;
+        font-size: 12pt;
+        font-weight: bold;
+        text-transform: uppercase;
+        margin-top: 0.5in;
+        text-align: center;
+        line-height: 1;
+      }
+    `;
+        document.head.appendChild(style);
     }
 }
 exports.default = ScriptFormattingPlugin;
--- test/script.md
+++ test/script.md
@@ -1,2 +1,1 @@
-<div class="scene-heading">Pepa</div>
-<div class="action">Cigan</div>
+# Krtke
