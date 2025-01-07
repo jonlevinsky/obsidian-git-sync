f000700bfb6882624b7465b766954108a6fb4433
jonlevinsky <124724335+jonlevinsky@users.noreply.github.com>
2025-01-07 18:18:18 +0100

    vault backup: 2025-01-07 18:18:17

--- .obsidian/plugins/celtx/script1.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.someFunction = someFunction;
-function someFunction() {
-    console.log("Funkce byla zavolána");
-}
