7f00c1618053a4831f83de3ca986a57761453343
jonlevinsky <124724335+jonlevinsky@users.noreply.github.com>
2025-01-07 20:04:17 +0100

    vault backup: 2025-01-07 20:04:17

--- .obsidian/plugins/celtx/main.js
+++ .obsidian/plugins/celtx/main.js
@@ -1,70 +1,42 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const obsidian_1 = require("obsidian");
-class ScriptFormattingPlugin extends obsidian_1.Plugin {
+class SceneScriptPlugin extends obsidian_1.Plugin {
     onload() {
         this.addCommand({
-            id: 'add-script-style-to-file',
-            name: 'Add Script Style to Current File',
-            callback: () => this.addScriptStyleToFile(),
+            id: 'apply-script-style',
+            name: 'Apply Script Style',
+            callback: () => this.applyScriptStyle(),
         });
     }
-    // Funkce pro přidání stylu do aktuálního souboru
-    async addScriptStyleToFile() {
-        const activeLeaf = this.app.workspace.activeLeaf;
-        if (!activeLeaf) {
-            new obsidian_1.Notice('No active leaf found.');
-            return;
-        }
-        const activeView = activeLeaf.view;
-        if (!activeView) {
-            new obsidian_1.Notice('No active view found.');
-            return;
-        }
-        const activeFile = this.getActiveFile(activeView);
-        if (activeFile && activeFile instanceof obsidian_1.TFile) {
-            // Načteme obsah souboru
-            const fileContent = await this.app.vault.read(activeFile);
-            // Zkontrolujeme, jestli soubor již obsahuje styl "script"
-            if (fileContent.includes('style: script')) {
-                new obsidian_1.Notice('File already has the script style.');
-                return;
-            }
-            // Přidáme do YAML frontmatter styl "script" (nebo další specifikace)
-            const newContent = this.addStyleToFrontmatter(fileContent);
-            // Uložíme změny do souboru
-            await this.app.vault.modify(activeFile, newContent);
-            new obsidian_1.Notice('Added script style to the file.');
+    applyScriptStyle() {
+        const activeView = this.app.workspace.activeLeaf?.view;
+        const activeFile = activeView instanceof obsidian_1.MarkdownView ? activeView.file : null;
+        if (activeFile && activeFile.extension === 'md') {
+            this.applyStyleToFile(activeFile);
         }
         else {
-            new obsidian_1.Notice('No active file to modify.');
+            new obsidian_1.Notice('No active Markdown file to modify.');
         }
     }
-    // Funkce pro získání souboru z view
-    getActiveFile(view) {
-        if (view instanceof obsidian_1.MarkdownView) {
-            return view.file;
-        }
-        return null;
+    applyStyleToFile(file) {
+        // Zkontrolujte, zda soubor obsahuje tag "style:script"
+        this.app.vault.read(file).then(content => {
+            if (content.includes('style:script')) {
+                // Přidání CSS souboru pouze pro tento soubor
+                this.applyCssToEditor();
+            }
+            else {
+                new obsidian_1.Notice('File does not have the style:script tag.');
+            }
+        });
     }
-    // Funkce pro přidání stylu do YAML frontmatter souboru
-    addStyleToFrontmatter(content) {
-        const frontmatterRegex = /^---\s*[\r\n](.*?)\s*[\r\n]---/s;
-        const frontmatterMatch = content.match(frontmatterRegex);
-        // Pokud soubor nemá frontmatter, přidáme ho
-        if (!frontmatterMatch) {
-            return `---
-style: script
----
-${content}`;
-        }
-        // Pokud frontmatter existuje, přidáme styl "script" nebo upravíme existující
-        const frontmatter = frontmatterMatch[1];
-        const newFrontmatter = frontmatter.includes('style: script')
-            ? frontmatter
-            : `${frontmatter}\nstyle: script`;
-        // Nahrazení starého frontmatter novým
-        return content.replace(frontmatterRegex, `---\n${newFrontmatter}\n---`);
+    applyCssToEditor() {
+        const styleLink = document.createElement('link');
+        styleLink.rel = 'stylesheet';
+        styleLink.href = './script.css';
+        document.head.appendChild(styleLink);
+        new obsidian_1.Notice('Applied script style to the file.');
     }
 }
-exports.default = ScriptFormattingPlugin;
+exports.default = SceneScriptPlugin;
--- test/script.md
+++ test/script.md
@@ -3,3 +3,5 @@ style: script
 ---
 
 # Krtke
+## Ahoj
+
